62089059b1e02027c5717d9a33f192179d45fd17
Gonzalo <gcarmenado@gmail.com>
2023-05-14 20:02:27 +0200

    Mas fotos y cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const sharp = require('sharp');
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
 const fs = require('fs');
 const path = require('path');
 
@@ -8,21 +6,18 @@ function defaultTask(cb) {
     cb();
     const imageFolder = path.join(__dirname, 'images');
     const images = fs.readdirSync(imageFolder);
-    console.log(images)
 
     images.forEach((image) => {
         const bufferArray = fs.readFileSync(path.join(imageFolder, image));
-        console.log(bufferArray);
-
         transformImages(bufferArray, image.split('.')[0]);
     });
-
 }
 
 
 async function transformImages(bufferArray, fileName) {
     sharp(bufferArray)
         .resize(1324, 744)
-        .toFile('reduced-images/' + fileName + '.avif', (err, info) => { console.log(info) });
+        .toFile('reduced-images/' + fileName + '.avif', (err, info) => {});
 }
+
 exports.default = defaultTask
